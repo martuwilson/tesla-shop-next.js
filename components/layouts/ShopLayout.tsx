e2c835d4af7bd615d1454a60d7b41cc84c34fe90
Martin Ezequiel Williner <williner.dev@gmail.com>
2023-11-12 14:25:35 -0300

    Add ShopLayout and Typography components to Home page

---
+++ components/layouts/ShopLayout.tsx
@@ -0,0 +1,49 @@
+import {FC} from 'react'
+import Head from "next/head"
+
+interface HeadProps {
+    children: React.ReactNode,
+    title: string,
+    pageDescription: string,
+    imageFullUrl?: string, // importante para compartir en redes sociales
+}
+
+export const ShopLayout:FC<HeadProps> = ({ children, title, pageDescription, imageFullUrl }) => {
+  return (
+    <>
+        <Head>
+            <title>{title}</title>
+            <meta name='description' content={pageDescription}/>
+
+            <meta name='og:title' content={title}/>
+            <meta name='og:description' content={pageDescription}/>
+
+            {
+                imageFullUrl && (
+                    <>
+                        <meta name='og:image' content={imageFullUrl}/>
+                    </>
+                )
+            }
+        </Head>
+
+        <nav>
+            {/* TODO NAVBAR */}
+        </nav>
+
+        {/* sidebar */}
+
+        <main style={{
+            margin: '80px auto',
+            maxWidth: '1440px',
+            padding: '0px 30px'
+        }}>
+            {children}
+        </main>
+
+        <footer>
+            {/* TODO FOOTER custom */}
+        </footer>
+    </>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
-
+import { ShopLayout } from "@/components/layouts";
+import { Typography } from "@mui/material";
 
 
 export default function Home() {
   return (
-    <>
-      <h1>
-        Hello world
-      </h1>
-    </>
+    <ShopLayout title="Teslo Shop || Home" pageDescription="Home page">
+      <Typography variant="h1" component='h1'>Tienda</Typography> {/* importante que sea H1 por el SEO */}
+      <Typography variant="h2" sx={{mb:1}}>Todos los productos</Typography>
+    </ShopLayout>
   )
 }
