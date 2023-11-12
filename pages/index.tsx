9bf1c79b07b1d0e3ed94975a76506716a46a2821
Martin Ezequiel Williner <williner.dev@gmail.com>
2023-11-12 18:19:38 -0300

    Home with products temporal added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import { ShopLayout } from "@/components/layouts";
-import { Typography } from "@mui/material";
+import { Typography, Grid,CardActionArea , CardMedia } from "@mui/material";
+
+import { initialData } from "@/database/products";
 
 
 export default function Home() {
@@ -7,6 +9,28 @@ export default function Home() {
     <ShopLayout title="Teslo Shop || Home" pageDescription="Home page">
       <Typography variant="h1" component='h1'>Tienda</Typography> {/* importante que sea H1 por el SEO */}
       <Typography variant="h2" sx={{mb:1}}>Todos los productos</Typography>
+
+
+
+      {/* home temporal */}
+
+      <Grid container spacing={4}>
+        {
+          initialData.products.map((product) => (
+            <Grid item xs={6} sm={4} key={product.slug}>
+              <CardActionArea>
+                <CardMedia
+                component="img"
+                image={`products/${product.images[0]}`}
+                alt={product.title}
+                />
+              </CardActionArea>
+            </Grid>
+          ))
+        }
+        
+      </Grid>
+
     </ShopLayout>
   )
 }
