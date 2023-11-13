7c447dd07151af415a848de3189cff30e87ecf7a
Martin Ezequiel Williner <williner.dev@gmail.com>
2023-11-13 11:10:54 -0300

    Add product images, links, and item counter to CartList component

--- components/cart/CartList.tsx
+++ components/cart/CartList.tsx
@@ -1,7 +1,9 @@
 
-import {Typography} from '@mui/material'
+import {Typography, Grid, CardActionArea, CardMedia, Box, Button} from '@mui/material'
 
 import { initialData } from "@/database/products"
+import Link from 'next/link'
+import { ItemCounter } from '../ui'
 
 
 const productsInCart = [
@@ -17,9 +19,47 @@ export const CartList = () => {
         {
             productsInCart.map( product => {
                 return (
-                    <Typography key={product.slug}>
-                        {product.title}
-                    </Typography>
+                    <Grid container spacing={2} key={product.slug} sx={{mb:1}}>
+                        
+                        <Grid item xs={3}>
+                            <Link
+                                href={`/product/slug`} /* ${product.slug} */
+                                passHref
+                            >
+                                    <CardActionArea>
+                                        <CardMedia
+                                            image={`products/${product.images[0]}`}
+                                            component={'img'}
+                                            sx={{borderRadius: '5px'}}
+                                        />
+                                    </CardActionArea>
+                            </Link>
+                        </Grid>
+
+                        <Grid item xs={7}>
+                            <Box display={'flex'} flexDirection={'column'}>
+                                <Typography variant='body1'>
+                                    {product.title}
+                                </Typography>
+                                <Typography variant='body1'>
+                                Talle: <strong>M</strong>
+                                </Typography> 
+                                {/* condicional */}
+                                <ItemCounter/>
+                            </Box>
+                        </Grid>
+
+                        <Grid item xs={2} display='flex' flexDirection='column' alignItems='center'>
+                            <Typography variant='subtitle1'>
+                                ${product.price}
+                            </Typography>
+                            {/* editable */}
+                            <Button variant='text' color="secondary" >
+                                Eliminar
+                            </Button>
+                        </Grid>
+
+                    </Grid>
                 )
             } )
         }
